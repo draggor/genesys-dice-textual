e36597d9ec4ac70af50480f61bf8a389509d0395
Draggor <draggor@gmail.com>
2024-10-23 20:38:04 -0500

    Refactoring, Result class, better typing, click for cli interface for now

--- dice.py
+++ dice.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass, field
 from enum import StrEnum
 import itertools
 import random
-from typing import List
+from typing import Any, Dict, List
 
 from pprint import pprint
+
+import click
 
 
 class Symbol(StrEnum):
@@ -49,7 +51,7 @@ class Die:
         self.faces = faces
 
     def roll(self) -> Face:
-      return random.choice(self.faces)
+        return random.choice(self.faces)
 
 
 Boost = Die(
@@ -146,152 +148,102 @@ dice_map = {
     Dice.PERCENTILE: Percentile,
 }
 
-
-@dataclass
-class Result:
-    totals = {
-        Symbol.TRIUMPH: 0,
-        Symbol.SUCCESS: 0,
-        Symbol.ADVANTAGE: 0,
-        Symbol.DESPAIR: 0,
-        Symbol.FAILURE: 0,
-        Symbol.THREAT: 0,
-        "Percentile": [],
-    }
-
 cancel_map = {
     Symbol.THREAT: Symbol.ADVANTAGE,
     Symbol.ADVANTAGE: Symbol.THREAT,
     Symbol.FAILURE: Symbol.SUCCESS,
     Symbol.SUCCESS: Symbol.FAILURE,
     Symbol.DESPAIR: Symbol.SUCCESS,
-    Symbol.TRIUMPH: Symbol.FAILURE
+    Symbol.TRIUMPH: Symbol.FAILURE,
 }
 
-def test_cancel(results, result):
-    match result:
-        case int():
-            results['Percentile'].append(result)
-        case Symbol():
-            opposite = cancel_map[result]
 
-            if result is Symbol.TRIUMPH or result is Symbol.DESPAIR:
-                result_add = cancel_map[opposite]
-                results[result] += 1
+@dataclass
+class Result:
+    totals: Dict[Any, Any]
+
+    results: List[Face] = field(default_factory=list)
+
+    def __init__(self, results: List[Face]) -> None:
+        self.totals = {
+            Symbol.TRIUMPH: 0,
+            Symbol.SUCCESS: 0,
+            Symbol.ADVANTAGE: 0,
+            Symbol.DESPAIR: 0,
+            Symbol.FAILURE: 0,
+            Symbol.THREAT: 0,
+            "Percentile": [],
+        }
+        self.results = results
+        self.reduce(results)
+
+    def reduce(self, results: List[Face]) -> None:
+        for result in results:
+            if type(result) is list:
+                for item in result:
+                    self.add(item)
             else:
-                result_add = result
+                self.add(result)
 
-            if results[opposite] == 0:
-                results[result_add] += 1
-            else:
-                results[opposite] -= 1
+    def add(self, result: Face) -> None:
+        match result:
+            case int():
+                self.totals["Percentile"].append(result)
+            case Symbol.BLANK:
+                pass
+            case Symbol():
+                opposite = cancel_map[result]
 
+                if result is Symbol.TRIUMPH or result is Symbol.DESPAIR:
+                    result_add = cancel_map[opposite]
+                    self.totals[result] += 1
+                else:
+                    result_add = result
 
+                if self.totals[opposite] == 0:
+                    self.totals[result_add] += 1
+                else:
+                    self.totals[opposite] -= 1
 
+    def __str__(self) -> str:
+        composed_str = ""
+        for symbol in [
+            Symbol.TRIUMPH,
+            Symbol.SUCCESS,
+            Symbol.ADVANTAGE,
+            Symbol.DESPAIR,
+            Symbol.FAILURE,
+            Symbol.THREAT,
+        ]:
+            composed_str += self.totals[symbol] * symbol_display[symbol]
 
-def add_result(results, result):
-    match result:
-        case int():
-            results["Percentile"].append(result)
-        case Symbol():
-            match result:
-                case Symbol.ADVANTAGE:
-                    if results[Symbol.THREAT] == 0:
-                        results[Symbol.ADVANTAGE] += 1
-                    else:
-                        results[Symbol.THREAT] -= 1
-                case Symbol.THREAT:
-                    if results[Symbol.ADVANTAGE] == 0:
-                        results[Symbol.THREAT] += 1
-                    else:
-                        results[Symbol.ADVANTAGE] -= 1
-                case Symbol.SUCCESS:
-                    if results[Symbol.FAILURE] == 0:
-                        results[Symbol.SUCCESS] += 1
-                    else:
-                        results[Symbol.FAILURE] -= 1
-                case Symbol.FAILURE:
-                    if results[Symbol.SUCCESS] == 0:
-                        results[Symbol.FAILURE] += 1
-                    else:
-                        results[Symbol.SUCCESS] -= 1
-                case Symbol.TRIUMPH:
-                    results[Symbol.TRIUMPH] += 1
-                    if results[Symbol.FAILURE] == 0:
-                        results[Symbol.SUCCESS] += 1
-                    else:
-                        results[Symbol.FAILURE] -= 1
-                case Symbol.DESPAIR:
-                    results[Symbol.DESPAIR] += 1
-                    if results[Symbol.SUCCESS] == 0:
-                        results[Symbol.FAILURE] += 1
-                    else:
-                        results[Symbol.SUCCESS] -= 1
+        composed_str = " ".join(composed_str)
+        composed_str += " " + " ".join(map(str, self.totals["Percentile"]))
 
-    return results
+        return composed_str
 
 
-def reduce_results(results: List[Face]):
-    reduced = {
-        Symbol.TRIUMPH: 0,
-        Symbol.SUCCESS: 0,
-        Symbol.ADVANTAGE: 0,
-        Symbol.DESPAIR: 0,
-        Symbol.FAILURE: 0,
-        Symbol.THREAT: 0,
-        "Percentile": [],
-    }
-
-    for result in results:
-        if type(result) is list:
-            for item in result:
-                add_result(reduced, item)
-        else:
-            add_result(reduced, result)
-
-    # return reduced
-    composed_str = ""
-    for symbol in [
-        Symbol.TRIUMPH,
-        Symbol.SUCCESS,
-        Symbol.ADVANTAGE,
-        Symbol.DESPAIR,
-        Symbol.FAILURE,
-        Symbol.THREAT,
-    ]:
-        composed_str += reduced[symbol] * symbol_display[symbol]
-
-    return composed_str
-
-
-# TODO: make results its own class for better tracking and future rendering
-def roll(dice_str: str) -> List[Face]:
+def roll(dice_str: str) -> str:
     results = []
 
     for die_str in dice_str:
         die = dice_map[die_str]
-        # TODO: roll should be a method on die
         # TODO: need to match result to die type for better display
         result = die.roll()
         results.append(result)
 
-    reduced = reduce_results(results)
-    pprint(results)
-
-    return ' '.join(reduced)
-
-
-pprint(roll('PAADDD%%'))
+    reduced = Result(results)
+    str_results = str(reduced)
+    return str_results
 
 
-def table(dice_str):
+def table(dice_str: str) -> Dict[Any, Any]:
     dice_faces = [dice_map[die_str].faces for die_str in dice_str]
     product = list(itertools.product(*dice_faces))
     total = len(product)
     reduced = {}
     for combo in product:
-        r = reduce_results(list(combo))
-        r = " ".join(r)
+        r = str(Result(list(combo)))
         if r in reduced:
             reduced[r] += 1
         else:
@@ -303,6 +255,27 @@ def table(dice_str):
     return reduced
 
 
-#t = table("PAADD")
-#pprint(len(t))
-#pprint(t)
+@click.command()
+@click.option("-t", is_flag=True, help="Print all rolls with probabilities")
+@click.argument("dice")
+def main(t, dice):
+    if t:
+        result = table(dice)
+
+        click.echo(len(result))
+        pprint(result)
+    else:
+        result = roll(dice)
+
+        click.echo(result)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# pprint(roll("PAADDD%%"))
+
+# t = table("PAADD")
+# pprint(len(t))
+# pprint(t)
