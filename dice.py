38b8f554c4e5a61fdbef6bfd31ca75bd5b320245
Draggor <draggor@gmail.com>
2024-10-28 02:21:43 -0500

    Added faces symbol distribution table

--- dice.py
+++ dice.py
@@ -299,8 +299,9 @@ def results_table(dice_str: str) -> Dict[Any, Any]:
 @click.command()
 @click.option("-t", is_flag=True, help="Print all rolls with probabilities")
 @click.option("-s", is_flag=True, help="Print the success rate of a roll")
-@click.argument("dice")
-def main(t, s, dice):
+@click.option("-f", is_flag=True, help="Print the faces of the dice")
+@click.argument("dice", required=False)
+def main(t, s, f, dice):
     """
     A dice roller and probablity calculator for the Genesys RPG system
 
@@ -338,10 +339,34 @@ def main(t, s, dice):
 
         console = Console()
         console.print(table)
-    else:
-        result = roll(dice)
+    elif f:
+        table = Table(title="Dice Faces")
+        table.add_column("Die", justify="right", style="cyan")
 
-        click.echo(result)
+        for i in range(1, 13):
+            table.add_column(str(i), justify="center", style="magenta")
+
+        for die_type, die in dice_map.items():
+            if die_type is Dice.PERCENTILE:
+                pass
+            else:
+                row = [die_type.name]
+                for face in die.faces:
+                    if type(face) is list:
+                        row.append(" ".join([symbol_display[f] for f in face]))
+                    else:
+                        row.append(symbol_display[face])
+                table.add_row(*row)
+
+        console = Console()
+        console.print(table)
+    else:
+        if dice is None:
+            click.echo(click.get_current_context().get_help())
+        else:
+            result = roll(dice)
+
+            click.echo(result)
 
 
 if __name__ == "__main__":
